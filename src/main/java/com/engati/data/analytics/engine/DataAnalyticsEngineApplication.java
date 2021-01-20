89ddd8798e40734b318ff062c1c686fddef74b4e
rishabhbohra3 <rishabhjain.bohra@gmail.com>
2021-01-20 17:28:46 +0530

    sprint jan - data analytics engine setup

---
+++ src/main/java/com/engati/data/analytics/engine/DataAnalyticsEngineApplication.java
@@ -0,0 +1,13 @@
+package com.engati.data.analytics.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAnalyticsEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataAnalyticsEngineApplication.class, args);
+	}
+
+}
