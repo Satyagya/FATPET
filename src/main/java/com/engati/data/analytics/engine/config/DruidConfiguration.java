61e0d86a67ed840fe5d4aeaa6564f9935560ef52
rishabhbohra3 <rishabhjain.bohra@gmail.com>
2021-01-21 13:28:00 +0530

    druid retrofit configuration

---
+++ src/main/java/com/engati/data/analytics/engine/config/DruidConfiguration.java
@@ -0,0 +1,15 @@
+package com.engati.data.analytics.engine.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "engati.action.server.druid")
+@Data
+public class DruidConfiguration {
+  private String url;
+  private Integer connectTimeout;
+  private Integer readTimeout;
+  private String logLevel;
+}
---
+++ src/main/java/com/engati/data/analytics/engine/config/RetrofitConfiguration.java
@@ -0,0 +1,38 @@
+package com.engati.data.analytics.engine.config;
+
+import com.engati.data.analytics.engine.retrofit.DruidServiceRetrofit;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class RetrofitConfiguration {
+
+  private static final ObjectMapper MAPPER = new ObjectMapper();
+  private static final Gson gson = new Gson();
+
+  @Bean(name = "retrofitDruidApi")
+  public Retrofit retrofitDruidApi(DruidConfiguration druidConfiguration) {
+    Retrofit.Builder builder = new Retrofit.Builder().baseUrl(druidConfiguration.getUrl()).client(
+        new OkHttpClient().newBuilder()
+            .connectTimeout(druidConfiguration.getConnectTimeout(), TimeUnit.MILLISECONDS)
+            .readTimeout(druidConfiguration.getReadTimeout(), TimeUnit.MILLISECONDS).build())
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create());
+    return builder.build();
+  }
+
+  @Bean(name = "retrofitEncoderApiService")
+  public DruidServiceRetrofit retrofitDruidApiService(
+      @Qualifier(value = "retrofitDruidApi") Retrofit retrofitDruidApi) {
+    return retrofitDruidApi.create(DruidServiceRetrofit.class);
+  }
+}
