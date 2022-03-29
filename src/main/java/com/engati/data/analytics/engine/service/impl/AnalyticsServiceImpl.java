30cf568cfefb09e4c3c6f722045a1429943e58c2
Jay Mehta <jay.mehta@engati.com>
2022-03-30 01:22:53 +0530

    exception handling for UnitSales and PurchaseHistory APIs

--- src/main/java/com/engati/data/analytics/engine/service/impl/AnalyticsServiceImpl.java
+++ src/main/java/com/engati/data/analytics/engine/service/impl/AnalyticsServiceImpl.java
@@ -28,45 +28,42 @@ public class AnalyticsServiceImpl implements AnalyticsService {
   @Override
   public DataAnalyticsResponse<List<ProductVariantResponse>> getVariantsByUnitSales(Long customerId, Long botRef, ProductDiscoveryRequest productDiscoveryRequest) {
     DataAnalyticsResponse<List<ProductVariantResponse>> response = new DataAnalyticsResponse<>();
-    List<ProductVariantResponse> responseList = new ArrayList<>();
+    try {
+      List<ProductVariantResponse> responseList = new ArrayList<>();
       String query = NativeQueries.PRODUCT_VARIANTS_BY_UNIT_SALES;
       query = query.replace(Constants.BOT_REF, botRef.toString());
       query = query.replace(QueryConstants.NUMBER_OF_RESULTS, productDiscoveryRequest.getNumberOfVariantsInResponse().toString());
-      if(productDiscoveryRequest.getCollection() != null){
-        query = query.replace(QueryConstants.ADD_COLLECTION_DETAILS_TO_QUERY, "and collection = '"+productDiscoveryRequest.getCollection()+"'");
+      if (productDiscoveryRequest.getCollection() != null) {
+        query = query.replace(QueryConstants.ADD_COLLECTION_DETAILS_TO_QUERY, "and collection = '" + productDiscoveryRequest.getCollection() + "'");
         query = query.replace(QueryConstants.ADD_PRODUCT_TYPE_TO_QUERY, QueryConstants.EMPTY_STRING);
         query = query.replace(QueryConstants.ADD_PRODUCT_TAG_TO_QUERY, QueryConstants.EMPTY_STRING);
         query = query.replace(QueryConstants.ADD_PRODUCT_IDS_TO_QUERY, QueryConstants.EMPTY_STRING);
-      }
-      else if (productDiscoveryRequest.getProductType() != null){
-        query = query.replace(QueryConstants.ADD_PRODUCT_TYPE_TO_QUERY, "and product_type = '"+productDiscoveryRequest.getProductType()+"'");
+      } else if (productDiscoveryRequest.getProductType() != null) {
+        query = query.replace(QueryConstants.ADD_PRODUCT_TYPE_TO_QUERY, "and product_type = '" + productDiscoveryRequest.getProductType() + "'");
         query = query.replace(QueryConstants.ADD_COLLECTION_DETAILS_TO_QUERY, QueryConstants.EMPTY_STRING);
         query = query.replace(QueryConstants.ADD_PRODUCT_TAG_TO_QUERY, QueryConstants.EMPTY_STRING);
         query = query.replace(QueryConstants.ADD_PRODUCT_IDS_TO_QUERY, QueryConstants.EMPTY_STRING);
 
-      }
-      else if (productDiscoveryRequest.getProductTag() != null){
-        query = query.replace(QueryConstants.ADD_PRODUCT_TAG_TO_QUERY, "and product_tags in '%"+productDiscoveryRequest.getProductTag()+"%'");
+      } else if (productDiscoveryRequest.getProductTag() != null) {
+        query = query.replace(QueryConstants.ADD_PRODUCT_TAG_TO_QUERY, "and product_tags in '%" + productDiscoveryRequest.getProductTag() + "%'");
         query = query.replace(QueryConstants.ADD_COLLECTION_DETAILS_TO_QUERY, QueryConstants.EMPTY_STRING);
         query = query.replace(QueryConstants.ADD_PRODUCT_TYPE_TO_QUERY, QueryConstants.EMPTY_STRING);
         query = query.replace(QueryConstants.ADD_PRODUCT_IDS_TO_QUERY, QueryConstants.EMPTY_STRING);
-      }
-      else if (!CollectionUtils.isEmpty(productDiscoveryRequest.getProductIds())){
+      } else if (!CollectionUtils.isEmpty(productDiscoveryRequest.getProductIds())) {
         query = query.replace(QueryConstants.ADD_PRODUCT_TAG_TO_QUERY, QueryConstants.EMPTY_STRING);
         query = query.replace(QueryConstants.ADD_COLLECTION_DETAILS_TO_QUERY, QueryConstants.EMPTY_STRING);
         query = query.replace(QueryConstants.ADD_PRODUCT_TYPE_TO_QUERY, QueryConstants.EMPTY_STRING);
-        query = query.replace(QueryConstants.ADD_PRODUCT_IDS_TO_QUERY, " and product_id in "+productDiscoveryRequest.getProductIds());
+        query = query.replace(QueryConstants.ADD_PRODUCT_IDS_TO_QUERY, " and product_id in " + productDiscoveryRequest.getProductIds());
         query = query.replace(QueryConstants.OPENING_SQUARE_BRACKET, QueryConstants.OPENING_ROUND_BRACKET);
         query = query.replace(QueryConstants.CLOSING_SQUARE_BRACKET, QueryConstants.CLOSING_ROUND_BRACKET);
-      }
-      else{
+      } else {
         query = query.replace(QueryConstants.ADD_COLLECTION_DETAILS_TO_QUERY, QueryConstants.EMPTY_STRING);
         query = query.replace(QueryConstants.ADD_PRODUCT_TYPE_TO_QUERY, QueryConstants.EMPTY_STRING);
         query = query.replace(QueryConstants.ADD_PRODUCT_TAG_TO_QUERY, QueryConstants.EMPTY_STRING);
         query = query.replace(QueryConstants.ADD_PRODUCT_IDS_TO_QUERY, QueryConstants.EMPTY_STRING);
       }
-    Map<Long, Map<String, Object>> productVariants = CommonUtils.executeQueryForDetails(query, Constants.VARIANT_ID);
-      for ( Map.Entry<Long, Map<String, Object>> productVariant : productVariants.entrySet()){
+      Map<Long, Map<String, Object>> productVariants = CommonUtils.executeQueryForDetails(query, Constants.VARIANT_ID);
+      for (Map.Entry<Long, Map<String, Object>> productVariant : productVariants.entrySet()) {
         Map<String, Object> variantMap = productVariant.getValue();
         ProductVariantResponse productVariantResponse = new ProductVariantResponse();
         productVariantResponse.setVariantId((Long) variantMap.get(Constants.VARIANT_ID));
@@ -75,55 +72,66 @@ public class AnalyticsServiceImpl implements AnalyticsService {
       }
       response.setResponseObject(responseList);
       response.setResponseStatusCode(ResponseStatusCode.SUCCESS);
-      return response;
+    }catch (Exception e) {
+      log.error("Exception encountered while fetching variants by Unit Sales for botRef: {}",botRef, e);
+      response.setResponseObject(null);
+      response.setResponseStatusCode(ResponseStatusCode.PROCESSING_ERROR);
+    }
+    return response;
   }
 
   @Override
   public DataAnalyticsResponse<List<OrderDetailsResponse>> getPurchaseHistory(PurchaseHistoryRequest purchaseHistoryRequest) {
     DataAnalyticsResponse<List<OrderDetailsResponse>> response = new DataAnalyticsResponse<>();
-    List<OrderDetailsResponse> responseList = new ArrayList<>();
-    String query = NativeQueries.PURCHASE_HISTORY;
-    query = query.replace(Constants.BOT_REF, purchaseHistoryRequest.getBotRef().toString());
+    try {
+      List<OrderDetailsResponse> responseList = new ArrayList<>();
+      String query = NativeQueries.PURCHASE_HISTORY;
+      query = query.replace(Constants.BOT_REF, purchaseHistoryRequest.getBotRef().toString());
 
-    if (purchaseHistoryRequest.getStartTime() == null && purchaseHistoryRequest.getEndTime() == null)
-      query = query.replace(QueryConstants.CREATED_AT_WINDOW, QueryConstants.EMPTY_STRING);
-    else if (purchaseHistoryRequest.getStartTime() == null && purchaseHistoryRequest.getEndTime() != null)
-      query = query.replace(QueryConstants.CREATED_AT_WINDOW, QueryConstants.CREATED_AT_LT_BASE + purchaseHistoryRequest.getEndTime().toString()+ QueryConstants.QUOTE_MARK);
-    else if (purchaseHistoryRequest.getStartTime() != null && purchaseHistoryRequest.getEndTime() == null)
-      query = query.replace(QueryConstants.CREATED_AT_WINDOW, QueryConstants.CREATED_AT_GT_BASE+ purchaseHistoryRequest.getStartTime().toString() + QueryConstants.QUOTE_MARK);
-    else {
-      query = query.replace(QueryConstants.FROM_DATE, QueryConstants.QUOTE_MARK+purchaseHistoryRequest.getStartTime().toString()+QueryConstants.QUOTE_MARK);
-      query = query.replace(QueryConstants.TO_DATE, QueryConstants.QUOTE_MARK+purchaseHistoryRequest.getEndTime().toString()+QueryConstants.QUOTE_MARK);
+      if (purchaseHistoryRequest.getStartTime() == null && purchaseHistoryRequest.getEndTime() == null)
+        query = query.replace(QueryConstants.CREATED_AT_WINDOW, QueryConstants.EMPTY_STRING);
+      else if (purchaseHistoryRequest.getStartTime() == null && purchaseHistoryRequest.getEndTime() != null)
+        query = query.replace(QueryConstants.CREATED_AT_WINDOW, QueryConstants.CREATED_AT_LT_BASE + purchaseHistoryRequest.getEndTime().toString() + QueryConstants.QUOTE_MARK);
+      else if (purchaseHistoryRequest.getStartTime() != null && purchaseHistoryRequest.getEndTime() == null)
+        query = query.replace(QueryConstants.CREATED_AT_WINDOW, QueryConstants.CREATED_AT_GT_BASE + purchaseHistoryRequest.getStartTime().toString() + QueryConstants.QUOTE_MARK);
+      else {
+        query = query.replace(QueryConstants.FROM_DATE, QueryConstants.QUOTE_MARK + purchaseHistoryRequest.getStartTime().toString() + QueryConstants.QUOTE_MARK);
+        query = query.replace(QueryConstants.TO_DATE, QueryConstants.QUOTE_MARK + purchaseHistoryRequest.getEndTime().toString() + QueryConstants.QUOTE_MARK);
+      }
+
+      if (!CollectionUtils.isEmpty(purchaseHistoryRequest.getCollection()))
+        query = query.replace(QueryConstants.COLLECTION_NAME, purchaseHistoryRequest.getCollection()
+            .stream().collect(Collectors.joining("','", "'", "'")));
+      else
+        query = query.replace(QueryConstants.COLLECTION_BASE, QueryConstants.EMPTY_STRING);
+
+      if (!CollectionUtils.isEmpty(purchaseHistoryRequest.getProductType()))
+        query = query.replace(QueryConstants.PRODUCT_TYPE, purchaseHistoryRequest.getProductType()
+            .stream().collect(Collectors.joining("','", "'", "'")));
+      else
+        query = query.replace(QueryConstants.PRODUCT_TYPE_BASE, QueryConstants.EMPTY_STRING);
+
+      Map<Long, Map<String, Object>> purchaseHistoryDetails = CommonUtils.executeQueryForDetails(query, QueryConstants.LINE_ITEM_ID);
+      for (Map.Entry<Long, Map<String, Object>> purchaseHistoryDetail : purchaseHistoryDetails.entrySet()) {
+        Map<String, Object> purchaseHistoryEntry = purchaseHistoryDetail.getValue();
+        OrderDetailsResponse orderDetailsResponse = new OrderDetailsResponse();
+        orderDetailsResponse.setOrderId((Long) purchaseHistoryEntry.get(QueryConstants.ORDER_ID));
+        orderDetailsResponse.setProductId((Long) purchaseHistoryEntry.get(QueryConstants.PRODUCT_ID));
+        orderDetailsResponse.setVariantId((Long) purchaseHistoryEntry.get(QueryConstants.VARIANT_ID));
+        orderDetailsResponse.setCustomerId((Long) purchaseHistoryEntry.get(QueryConstants.CUSTOMER_ID));
+        orderDetailsResponse.setCollectionId((Long) purchaseHistoryEntry.get(QueryConstants.COLLECTION_ID));
+        orderDetailsResponse.setBotRef((Long) purchaseHistoryEntry.get(QueryConstants.BOT_REF));
+        orderDetailsResponse.setCreatedDate(String.valueOf(purchaseHistoryEntry.get(QueryConstants.CREATED_AT)));
+        orderDetailsResponse.setPrice((Double) purchaseHistoryEntry.get(QueryConstants.LINE_ITEM_PRICE));
+        responseList.add(orderDetailsResponse);
+      }
+      response.setResponseObject(responseList);
+      response.setResponseStatusCode(ResponseStatusCode.SUCCESS);
+    }catch (Exception e){
+      log.error("Exception encountered while fetching purchase history for botRef: ",purchaseHistoryRequest.getBotRef(), e);
+      response.setResponseObject(null);
+      response.setResponseStatusCode(ResponseStatusCode.PROCESSING_ERROR);
     }
-
-    if (!CollectionUtils.isEmpty(purchaseHistoryRequest.getCollection()))
-      query = query.replace(QueryConstants.COLLECTION_NAME, purchaseHistoryRequest.getCollection()
-          .stream().collect(Collectors.joining("','", "'", "'")));
-    else
-      query = query.replace(QueryConstants.COLLECTION_BASE, QueryConstants.EMPTY_STRING);
-
-    if (!CollectionUtils.isEmpty(purchaseHistoryRequest.getProductType()))
-      query = query.replace(QueryConstants.PRODUCT_TYPE, purchaseHistoryRequest.getProductType()
-          .stream().collect(Collectors.joining("','", "'", "'")));
-    else
-      query = query.replace(QueryConstants.PRODUCT_TYPE_BASE, QueryConstants.EMPTY_STRING);
-
-    Map<Long, Map<String, Object>> purchaseHistoryDetails = CommonUtils.executeQueryForDetails(query, QueryConstants.LINE_ITEM_ID);
-    for (Map.Entry<Long, Map<String, Object>> purchaseHistoryDetail : purchaseHistoryDetails.entrySet()) {
-      Map<String, Object> purchaseHistoryEntry = purchaseHistoryDetail.getValue();
-      OrderDetailsResponse orderDetailsResponse = new OrderDetailsResponse();
-      orderDetailsResponse.setOrderId((Long) purchaseHistoryEntry.get(QueryConstants.ORDER_ID));
-      orderDetailsResponse.setProductId((Long) purchaseHistoryEntry.get(QueryConstants.PRODUCT_ID));
-      orderDetailsResponse.setVariantId((Long) purchaseHistoryEntry.get(QueryConstants.VARIANT_ID));
-      orderDetailsResponse.setCustomerId((Long) purchaseHistoryEntry.get(QueryConstants.CUSTOMER_ID));
-      orderDetailsResponse.setCollectionId((Long) purchaseHistoryEntry.get(QueryConstants.COLLECTION_ID));
-      orderDetailsResponse.setBotRef((Long) purchaseHistoryEntry.get(QueryConstants.BOT_REF));
-      orderDetailsResponse.setCreatedDate(String.valueOf(purchaseHistoryEntry.get(QueryConstants.CREATED_AT)));
-      orderDetailsResponse.setPrice((Double) purchaseHistoryEntry.get(QueryConstants.LINE_ITEM_PRICE));
-      responseList.add(orderDetailsResponse);
-    }
-    response.setResponseObject(responseList);
-    response.setResponseStatusCode(ResponseStatusCode.SUCCESS);
     return response;
   }
 }
