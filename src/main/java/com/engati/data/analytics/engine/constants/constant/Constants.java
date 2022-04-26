9d32cf4a9677377832f9eaedd4f347f8fa31bacd
Jay Mehta <jay.mehta@engati.com>
2022-04-26 14:54:30 +0530

    duckdb to fastapi changes

--- src/main/java/com/engati/data/analytics/engine/constants/constant/Constants.java
+++ src/main/java/com/engati/data/analytics/engine/constants/constant/Constants.java
@@ -5,14 +5,18 @@ import java.util.Locale;
 public class Constants {
 
   public static final String RESPONSE_OBJECT = "response";
-  public static final String CSV_BASE_PATH_FORMAT = "/opt/engati/customer_segments/%s";
-  public static final String CSV_PATH_FORMAT = "/opt/engati/customer_segments/%s/%s.csv";
+//  public static final String CSV_BASE_PATH_FORMAT = "/opt/engati/customer_segments/%s";
+//  public static final String CSV_PATH_FORMAT = "/opt/engati/customer_segments/%s/%s.csv";
+  public static final String CSV_BASE_PATH_FORMAT = "/Users/jaymehta/Desktop/IdeaProjects/data-analytics-engine/src/main/resources/customer_segments/%s";
+  public static final String CSV_PATH_FORMAT = "/Users/jaymehta/Desktop/IdeaProjects/data-analytics-engine/src/main/resources/customer_segments/%s/%s.csv";
+
+
 
   //  DuckDB Connection URL
   public static String DUCKDB_CONNECTION_URI = "jdbc:duckdb:";
 //  public static String PAQUET_FILE_PATH = "/opt/engati/shopify-etl-engine/parquet_store";
-  public static String PARQUET_FILE_PATH = "/opt/engati/parquet_store";
-//  public static String PARQUET_FILE_PATH = "/Users/jaymehta/Desktop/PycharmProjects/shopify-etl-engine/parquet_store";
+//  public static String PARQUET_FILE_PATH = "/opt/engati/parquet_store";
+  public static String PARQUET_FILE_PATH = "/Users/jaymehta/Desktop/PycharmProjects/shopify-etl-engine/parquet_store";
 
 //  Defaults
   public static Long DEFAULT_BOTREF = -1L;
@@ -29,12 +33,12 @@ public class Constants {
   public static String PRODUCT_ID = "product_id";
 
   // Retrofit
-  public static final String ETL_ENGINE_PREFIX = "etl.engine";
+  public static final String DUCKDB_ENGINE_PREFIX = "duckdb.engine";
   public static final String DUCK_DB_EXECUTE_QUERY = "/v1.0/api/duckdb/query/";
   public static final String DUCK_DB_EXECUTE_QUERY_DETAILS = "/v1.0/api/duckdb/query/details/";
-  public static final String RETROFIT_ETL_ENGINE_API = "retrofitEtlEngineApi";
-  public static final String ETL_ENGINE_HTTP_CLIENT = "EtlEngineHttpClient";
-  public static final String RETROFIT_ETL_ENGINE_REST_SERVICE = "retrofitEtlEngineRestService";
+  public static final String RETROFIT_DUCKDB_ENGINE_API = "retrofitDuckdbEngineApi";
+  public static final String DUCKDB_ENGINE_HTTP_CLIENT = "DuckdbEngineHttpClient";
+  public static final String RETROFIT_DUCKDB_ENGINE_REST_SERVICE = "retrofitDuckdbEngineRestService";
 
   public static final String QUERY = "query";
   public static final String KEY = "key";
