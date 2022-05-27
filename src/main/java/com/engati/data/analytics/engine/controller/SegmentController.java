b0978352367f1ee82cf1f1355c350b491bd28d49
Jay Mehta <jay.mehta@engati.com>
2022-05-27 15:31:05 +0530

    Custom Segment changes

--- src/main/java/com/engati/data/analytics/engine/controller/SegmentController.java
+++ src/main/java/com/engati/data/analytics/engine/controller/SegmentController.java
@@ -2,15 +2,14 @@ package com.engati.data.analytics.engine.controller;
 
 import com.engati.data.analytics.engine.common.model.DataAnalyticsResponse;
 import com.engati.data.analytics.engine.constants.constant.ApiPathConstants;
+import com.engati.data.analytics.engine.model.request.CustomSegmentRequest;
 import com.engati.data.analytics.engine.model.response.CustomerSegmentationResponse;
 import com.engati.data.analytics.engine.service.SegmentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 
@@ -22,13 +21,19 @@ public class SegmentController {
   @Autowired
   private SegmentService segmentService;
 
-  @GetMapping(value = ApiPathConstants.GET_CUSTOMER_SEGMENT)
-  public ResponseEntity<DataAnalyticsResponse> getCustomerSegment(
-      @PathVariable(name = ApiPathConstants.BOTREF) Long botRef,
-      @PathVariable(name = ApiPathConstants.SEGMENT_NAME) String segmentName ) {
+  @GetMapping(value = ApiPathConstants.GET_SYSTEM_SEGMENT)
+  public ResponseEntity<DataAnalyticsResponse> getSystemSegment(@PathVariable(name = ApiPathConstants.BOTREF) Long botRef, @PathVariable(name = ApiPathConstants.SEGMENT_NAME) String segmentName) {
     log.info("Got call for getCustomerSegment for botRef: {}, segmentName: {}", botRef, segmentName);
-      DataAnalyticsResponse<List<CustomerSegmentationResponse>> response = segmentService.getCustomersForSegment(botRef, segmentName);
-        return new ResponseEntity<>(response, response.getStatusCode());
+    DataAnalyticsResponse<List<CustomerSegmentationResponse>> response = segmentService.getCustomersForSystemSegment(botRef, segmentName);
+    return new ResponseEntity<>(response, response.getStatusCode());
+  }
+
+
+  @GetMapping(value = ApiPathConstants.GET_CUSTOM_SEGMENT)
+  public ResponseEntity<DataAnalyticsResponse> getCustomSegment(@PathVariable(name = ApiPathConstants.BOTREF) Long botRef, @RequestBody CustomSegmentRequest customSegmentRequest) {
+    log.info("Got call for getCustomerSegment for botRef: {}, segmentCondition: {}", botRef, customSegmentRequest.getSegmentCondition());
+    DataAnalyticsResponse<List<CustomerSegmentationResponse>> response = segmentService.getCustomersForCustomSegment(botRef, customSegmentRequest.getSegmentCondition());
+    return new ResponseEntity<>(response, response.getStatusCode());
   }
 
 }
