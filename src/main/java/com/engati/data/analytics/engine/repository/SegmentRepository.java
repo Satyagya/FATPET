f6e300c9f6cb999d9d2c21c785c7331929abf824
Jay Mehta <jay.mehta@engati.com>
2022-06-22 00:39:21 +0530

    null email fix

--- src/main/java/com/engati/data/analytics/engine/repository/SegmentRepository.java
+++ src/main/java/com/engati/data/analytics/engine/repository/SegmentRepository.java
@@ -13,7 +13,7 @@ import java.util.Set;
 @Repository("com.engati.broadcast.repository.SegmentsRepository")
 public interface SegmentRepository extends JpaRepository<ShopifyCustomer, Long> {
 
-  @Query(value = "select customer_id, customer_email, customer_phone,\n" +
+  @Query(value = "select customer_id, coalesce(customer_email, '')as customer_email, customer_phone,\n" +
       "       concat(COALESCE(first_name, \"\") ,' ', COALESCE(last_name, \"\"))as customer_name " +
       "from shopify_customer " +
       "where customer_id in :customerIds ", nativeQuery = true)
