047b8c9044efb97bff4d85499aabeae8d997aeed
swastik.k <swastik.k@engati.com>
2022-09-07 15:56:52 +0530

    added @JsonIgnoreProperties(ignoreUnknown = true) to PdeProductResponse

--- src/main/java/com/engati/data/analytics/engine/model/response/PdeProductResponse.java
+++ src/main/java/com/engati/data/analytics/engine/model/response/PdeProductResponse.java
@@ -1,11 +1,13 @@
 package com.engati.data.analytics.engine.model.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 import lombok.Data;
 
 
 @Data
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PdeProductResponse {
 
   private Long PRODUCT_productId;
