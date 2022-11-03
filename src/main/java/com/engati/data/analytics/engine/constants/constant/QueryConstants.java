9f516ffd2774835443f3f421325142bbabad2a85
satyagya.kumar <satyagya.kumar@engati.com>
2022-11-03 15:36:21 +0530

    8260-Satya-Refactored

--- src/main/java/com/engati/data/analytics/engine/constants/constant/QueryConstants.java
+++ src/main/java/com/engati/data/analytics/engine/constants/constant/QueryConstants.java
@@ -65,18 +65,6 @@ public class QueryConstants {
   public static String TRANSACTION_REVENUE = "transaction_revenue";
   public static String AMOUNT_SPENT = "amount_spent";
   public static String PRODUCT_TYPES = "ProductTypes";
-<<<<<<< HEAD
-
-=======
-  public static String COLLECTIONS = "collections";
-  public static String CITIES = "cities";
-  public static String COUNTRIES = "countries";
   public static String SUBSCRIPTION_TITLES = "subscriptionTitles";
-
-
-
-
-
-
 
 }
