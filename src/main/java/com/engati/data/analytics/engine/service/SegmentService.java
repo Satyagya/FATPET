c19c6509d21ce4e3faafff762d9d8f14e3ec5d7d
Aniket Modi <aniket.modi@engati.com>
2022-11-09 11:09:44 +0000

    Update SegmentService.java

--- src/main/java/com/engati/data/analytics/engine/service/SegmentService.java
+++ src/main/java/com/engati/data/analytics/engine/service/SegmentService.java
@@ -14,8 +14,5 @@ public interface SegmentService {
     DataAnalyticsResponse<List<CustomerSegmentationCustomSegmentResponse>> getCustomersForCustomSegment(Long botRef, CustomSegmentRequest customSegmentRequest);
 
     DataAnalyticsResponse<List<String>> getProductType(Long botRef);
-    DataAnalyticsResponse<List<String>> getCollection(Long botRef);
-    DataAnalyticsResponse<List<String>> getCountry(Long botRef);
-    DataAnalyticsResponse<List<String>> getCity(Long botRef);
 
 }
