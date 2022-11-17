5d9b8b0158a8870e47b80e40c77e957ff91c4682
Aniket Modi <aniket.modi@engati.com>
2022-11-17 19:59:42 +0530

    EEA-2702-one-merge-conflict-resolved

--- src/main/java/com/engati/data/analytics/engine/service/SegmentService.java
+++ src/main/java/com/engati/data/analytics/engine/service/SegmentService.java
@@ -14,11 +14,8 @@ public interface SegmentService {
     DataAnalyticsResponse<List<CustomerSegmentationCustomSegmentResponse>> getCustomersForCustomSegment(Long botRef, CustomSegmentRequest customSegmentRequest);
 
     DataAnalyticsResponse<List<String>> getProductType(Long botRef);
-<<<<<<< HEAD
     DataAnalyticsResponse<List<String>> getCollection(Long botRef);
     DataAnalyticsResponse<List<String>> getCountry(Long botRef);
     DataAnalyticsResponse<List<String>> getCity();
-=======
->>>>>>> 715640a7d20cafd002ab7ba0ef679767ccbba49e
 
 }
